bb620a4a887a892dda2a82e9a18e793f1226284f
kjkt <kkjjimp@gmail.com>
2025-10-25 17:20:07 +0900

    webfluxのデモを追加

--- lab-webflux/src/main/java/com/example/lab_webflux/api/HelloApiController.java
+++ lab-webflux/src/main/java/com/example/lab_webflux/api/HelloApiController.java
@@ -1,4 +1,17 @@
 package com.example.lab_webflux.api;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
+@RestController
 public class HelloApiController {
+
+    @GetMapping("/api/hello-reactive")
+    public Mono<String> helloReactive() {
+        return Mono.delay(Duration.ofMillis(200)).then(Mono.just("Hello Webflux!!"));
+    }
+
 }
