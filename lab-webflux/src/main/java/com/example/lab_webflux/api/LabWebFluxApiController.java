b304dddbc4ca0bcae14e49782b249fbbb3d1e6b4
kjkt <kkjjimp@gmail.com>
2025-10-27 22:42:03 +0900

    WebFluxデモページを作成

--- lab-webflux/src/main/java/com/example/lab_webflux/api/LabWebFluxApiController.java
+++ lab-webflux/src/main/java/com/example/lab_webflux/api/LabWebFluxApiController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @RestController
-public class HelloApiController {
+public class LabWebFluxApiController {
 
   @GetMapping("/api/hello-reactive")
   public Mono<String> helloReactive() {
--- lab-webflux/src/main/java/com/example/lab_webflux/controller/LabWebFluxController.java
+++ lab-webflux/src/main/java/com/example/lab_webflux/controller/LabWebFluxController.java
@@ -4,9 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class IndexController {
+public class LabWebFluxController {
+
   @GetMapping("/")
   public String index() {
     return "index";
+  }
+
+  @GetMapping("/lab-webflux")
+  public String labWebFlux() {
+
+    return "lab-webflux";
   }
 }
